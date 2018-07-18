45e6daa69894f8b3e7525407ac53f298d0e32917
miyoshida <miyoshida@aiming-inc.com>
2018-07-18 18:16:03 +0900

    SelectSwitchのテストを追加

---
+++ Assets/Button/ButtonEx.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonEx : Button
+{
+
+    protected override void DoStateTransition(SelectionState state, bool instant)
+    {
+        base.DoStateTransition(state, instant);
+        Debug.Log("state : " + state);
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+        Debug.Log("onDeselect : " + eventData.selectedObject.name);
+    }
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+        Debug.Log("OnSelect : " + eventData.selectedObject.name);
+    }
+
+    public override void OnSubmit(BaseEventData eventData)
+    {
+        base.OnSubmit(eventData);
+        Debug.Log("OnSubmit : " + eventData.selectedObject.name);
+    }
+}
---
+++ Assets/TestScene/TestSceneMain.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestSceneMain : MonoBehaviour
+{
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
+
+    readonly ISubject<Unit> request = new Subject<Unit>();
+
+    ReactiveProperty<bool> boolRP = new ReactiveProperty<bool>();
+
+    // Use this for initialization
+    void Start()
+    {
+        boolRP.Value = true;
+
+        button1.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                request.OnNext(Unit.Default);
+            }).AddTo(this);
+
+
+        request.Select(_ => CoroutineTaskAsync().TakeUntil(boolRP.Where(b => !b)))
+            .Switch()
+            .Subscribe()
+            .AddTo(this);
+
+
+        button2.OnClickAsObservable()
+            .Subscribe(_ =>
+            {
+                boolRP.Value = !boolRP.Value;
+            }).AddTo(this);
+    }
+
+
+    IObservable<Unit> CoroutineTaskAsync()
+    {
+        return Observable.FromCoroutine(() => CoroutineTaskCore());
+    }
+
+
+    IEnumerator CoroutineTaskCore()
+    {
+
+
+        while (true)
+        {
+
+            yield return new WaitForSeconds(0.2f);
+            Debug.Log("aaaa");
+        }
+
+
+    }
+
+}
